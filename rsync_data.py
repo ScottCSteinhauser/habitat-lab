bab237fc8ecc2cbca4d9e84db1f7b09ec643678a
Alexander William Clegg <alexclegg@fb.com>
2022-02-09 11:53:25 -0800

    add utilities for running jobs and uploading/downloading results

---
+++ rsync_data.py
@@ -0,0 +1,50 @@
+import os
+import argparse
+
+rsync_base = "rsync -azhP "
+hab_lab_local_path = "/Users/alexclegg/Documents/dev2/scott_ant_task/habitat-lab/"
+hab_lab_remote_path = "alexclegg@devfair:~/scott_ant_dir/habitat-lab/"
+sub_directories = {
+    "results": ["data/checkpoints/", "data/tb/"],
+    "hab_lab_code": ["configs/", "habitat/", "habitat_baselines/", "run_exp.py"],
+    "config_only": ["habitat_baselines/config/ant_v2/"],
+}
+
+directions = ["up", "down"]
+types = ["results", "code", "config"]
+
+def rsync_remote(direction="down", type="results"):
+    assert direction in directions
+    assert type in types
+
+    full_command = rsync_base
+    local_path = hab_lab_local_path
+    remote_path = hab_lab_remote_path
+    target_sub_directories = sub_directories["results"]
+    if type == "code":
+        target_sub_directories = sub_directories["hab_lab_code"]
+    if type == "config":
+        target_sub_directories = sub_directories["config_only"]
+
+    for sub_dir in target_sub_directories:
+        final_command = full_command + remote_path + sub_dir + " " + local_path + sub_dir
+        if direction == "up":
+            final_command = full_command + local_path + sub_dir + " " + remote_path + sub_dir
+        print(final_command)
+        os.system(final_command)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--up", action="store_true")
+    parser.add_argument("--code", action="store_true")
+    parser.add_argument("--config", action="store_true")
+    #default is 'download' 'results' from remote
+    #optional params change direction and upload type
+    args = parser.parse_args()
+    direction = "down" if not args.up else "up"
+    type = "results"
+    if args.config:
+        type="config"
+    elif args.code:
+        type="code"
+    rsync_remote(direction, type)
---
+++ run_exp.py
@@ -0,0 +1,94 @@
+import os
+import argparse
+from typing import Dict
+
+#--------------------------------------------------------
+# Wrapper around habitat_baselines/run.py 
+# Encapsulates experiment definitions and common overrides.
+#--------------------------------------------------------
+
+# List experiments for quick multi-launch from commandline.
+#NOTE: See the examples/template below for formatting
+experiments: Dict[str, Dict[str,str]] = {
+    # "template":{
+    #     "description": "",
+    #     "config": "",
+    #     "overrides": "",
+    # }
+    # "example_name": {
+    #     "description": "Example: describe your experiment for later.",
+    #     "config": "Example: habitat_baselines/config/ant_v2/ppo_ant_v2_eval.yaml",
+    #     "overrides": "Example: CHECKPOINT_FOLDER 'data/new_checkpoints_video_enabled_3'"
+    # },
+    "x_loc_ant": {
+        "description": "Reward cumulative progress in X direction.",
+        "config": "habitat_baselines/config/ant_v2/ppo_ant_v2_train.yaml",
+        "overrides": "",
+    },
+    "joint_error_ant":{
+        "description": "Linear penalty for target joint angle error.",
+        "config": "habitat_baselines/config/ant_v2/ppo_ant_v2_train.yaml",
+        "overrides": " RL.REWARD_MEASURE JOINT_STATE_ERROR",
+    }
+}
+
+
+run_types = ["eval", "train"]
+run_base = "python -u habitat_baselines/run.py"
+
+
+def run(experiment=None, run_type="train"):
+    assert experiment in experiments
+    assert run_type in run_types
+
+    exp_info = experiments[experiment]
+
+    full_command = run_base
+    
+    #add the config
+    full_command += " --exp-config " + exp_info["config"]
+    #add the type
+    full_command += " --run-type " + run_type
+
+    overrides = ""
+
+    #add overrides for exp name
+    #NOTE: storing results in data/checkpoints/<expriment_name>/ and data/tb/<expriment_name>/
+    overrides += " TENSORBOARD_DIR data/tb/" + experiment + "/"
+    overrides += " CHECKPOINT_FOLDER data/checkpoints/" + experiment + "/"
+    overrides += exp_info["overrides"]
+
+    #add known type overrides for eval
+    if run_type == "eval":
+        #overrides += " VIDEO_OPTION "
+        overrides += " EVAL_CKPT_PATH_DIR data/checkpoints/" + experiment + "/"
+        overrides += " VIDEO_DIR data/videos/" + experiment + "/"
+        #NOTE: this adds the extra sensor for visualization
+        overrides += " SENSORS ['THIRD_SENSOR']"
+        #NOTE: number of videos/data_points you want
+        overrides += " NUM_ENVIRONMENTS 3"
+
+    #add the overrides
+    full_command += overrides
+
+    #NOTE: comment out the final line to check before running if in doubt
+    print(full_command)
+    os.system(full_command)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--type",
+        choices=["train", "eval"],
+        required=True,
+        help="run type of the experiment (train or eval)",
+    )
+    parser.add_argument(
+        "--exp",
+        type=str,
+        required=True,
+        help="name of the experiment as defined in the experiments dict",
+    )
+
+    args = parser.parse_args()
+    run(experiment=args.exp, run_type=args.type)
