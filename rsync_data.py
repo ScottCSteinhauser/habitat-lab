5aac853a918a5f5403cd1f32f0d07d000c0d6dde
Alexander William Clegg <alexclegg@fb.com>
2022-02-10 12:38:07 -0800

    add local results clean-up with rsync.py --clean

--- rsync_data.py
+++ rsync_data.py
@@ -5,6 +5,8 @@ rsync_base = "rsync -azhP "
 hab_lab_local_path = "/Users/alexclegg/Documents/dev2/scott_ant_task/habitat-lab/"
 hab_lab_remote_path = "alexclegg@devfair:~/scott_ant_dir/habitat-lab/"
 sub_directories = {
+    "archive": ["data/ant_exp_archive/"],
+    "videos": ["data/videos/"],
     "results": ["data/checkpoints/", "data/tb/"],
     "hab_lab_code": ["configs/", "habitat/", "habitat_baselines/", "run_exp.py"],
     "config_only": ["habitat_baselines/config/ant_v2/"],
@@ -33,18 +35,33 @@ def rsync_remote(direction="down", type="results"):
         print(final_command)
         os.system(final_command)
 
+def clean_data():
+    rm_base = "rm -r "
+    full_command = rm_base
+    target_sub_directories = sub_directories["results"] + sub_directories["videos"]
+    for sub_dir in target_sub_directories:
+        final_command = full_command + hab_lab_local_path + sub_dir
+        print(final_command)
+        os.system(final_command)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--clean", action="store_true")
     parser.add_argument("--up", action="store_true")
     parser.add_argument("--code", action="store_true")
     parser.add_argument("--config", action="store_true")
     #default is 'download' 'results' from remote
     #optional params change direction and upload type
     args = parser.parse_args()
-    direction = "down" if not args.up else "up"
-    type = "results"
-    if args.config:
-        type="config"
-    elif args.code:
-        type="code"
-    rsync_remote(direction, type)
+
+    if args.clean:
+        clean_data()
+    else:
+        direction = "down" if not args.up else "up"
+        type = "results"
+        if args.config:
+            type="config"
+        elif args.code:
+            type="code"
+        rsync_remote(direction, type)
