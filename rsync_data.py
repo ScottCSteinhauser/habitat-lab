ac93e26b9dc95daa5ca6b8b107636110f2ca8617
Alexander William Clegg <alexclegg@fb.com>
2022-02-11 11:07:21 -0800

    add --archive <archive_path> option to rsync.py

--- rsync_data.py
+++ rsync_data.py
@@ -8,7 +8,7 @@ sub_directories = {
     "archive": ["data/ant_exp_archive/"],
     "videos": ["data/videos/"],
     "results": ["data/checkpoints/", "data/tb/"],
-    "hab_lab_code": ["configs/", "habitat/", "habitat_baselines/", "run_exp.py"],
+    "hab_lab_code": ["configs/", "habitat/", "habitat_baselines/", "run_exp.py", "rsync.py"],
     "config_only": ["habitat_baselines/config/ant_v2/"],
 }
 
@@ -44,10 +44,35 @@ def clean_data():
         print(final_command)
         os.system(final_command)
 
+def archive_data(archive_directory):
+    rsync_base = "rsync -azhP "
+    target_sub_directories = sub_directories["results"] + sub_directories["videos"]
+    target_sub_directories.append("run_exp.py")
+    target_sub_directories.append("habitat_baselines/config/ant_v2/ppo_ant_v2_train.yaml")
+    target_sub_directories.append("configs/tasks/ant-v2.yaml")
+    target_sub_directories.append("habitat/tasks/ant_v2/ant_v2.py")
+    for sub_dir in target_sub_directories:
+        source = hab_lab_local_path + sub_dir
+        destination = archive_directory
+        if sub_dir.endswith("/"):
+            destination = archive_directory+sub_dir.split("/")[-2] + "/"
+        final_command = rsync_base + source + " " + destination
+        #print(f"source = {source}")
+        #print(f"destination = {destination}")
+        if not os.path.exists(destination):
+            create_dir_command = "mkdir -p " + destination
+            print(f"create_dir_command = {create_dir_command}")
+            os.system(create_dir_command)
+        print(f"final_command = {final_command}")
+        os.system(final_command)
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--clean", action="store_true")
+    parser.add_argument("--archive", type=str,
+        required=False,
+        help="If provided, copy all data to a specified directory. Overrides other options.",)
     parser.add_argument("--up", action="store_true")
     parser.add_argument("--code", action="store_true")
     parser.add_argument("--config", action="store_true")
@@ -55,7 +80,9 @@ if __name__ == "__main__":
     #optional params change direction and upload type
     args = parser.parse_args()
 
-    if args.clean:
+    if args.archive:
+        archive_data(args.archive)
+    elif args.clean:
         clean_data()
     else:
         direction = "down" if not args.up else "up"
